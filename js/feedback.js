dd9173c9df2b95e0092100e8475fe137c647ffbf
steve <909476821@qq.com>
2021-11-03 14:21:04 +0800

    增加feedback埋点

--- js/feedback.js
+++ js/feedback.js
@@ -1,13 +1,8 @@
-var _czc = _czc || [];
-_czc.push(["_setAccount","1280517875"]);
-
-var url = window.location.href;
-
 function plugin (hook, vm) {
   hook.beforeEach(function(html) {
 
     url = window.location.href;
-    var editHtml = '<div><span>该文档内容是否给您带来了帮助？</span><button type="button" onclick="youyongClick()">有用</button><button type="button" onclick="meiyongClick()">没用</button></div>';
+    var editHtml = '<div><span>该文档内容是否给您带来了帮助？</span><button type="button" onclick="helpfulClick()">有用</button><button type="button" onclick="unhelpfulClick()">没用</button></div>';
 
     return (
       html + '</br></br></br></br>' + editHtml
@@ -15,14 +10,16 @@ function plugin (hook, vm) {
   });
 }
 
-function youyongClick() {
-  _czc.push(['_traceEvent','feedback','zan',url,1,'zan']);
-  alert("感谢您的反馈");
+function helpfulClick() {
+
+	var url = window.location.href;
+	_czc.push(["_trackEvent", "feedback-helpful", "helpful", url]);
+    alert("感谢您的反馈");
 }
 
-function meiyongClick() {
-  _czc.push(['_traceEvent','feedback','nozan',url,2,'nozan']);
-  alert("感谢您的反馈");
+function unhelpfulClick() {
+    _czc.push(['_trackEvent','feedback-unhelpful','unhelpful', url]);
+    alert("感谢您的反馈");
 }
 
 window.$docsify.plugins = [].concat(plugin, window.$docsify.plugins);
