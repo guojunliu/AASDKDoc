7398fd550f625ea30dfe158934262e7b0236d302
steve <909476821@qq.com>
2021-11-04 11:08:27 +0800

    更新统计代码

--- js/feedback.js
+++ js/feedback.js
@@ -14,14 +14,13 @@ function plugin (hook, vm) {
 
 function helpfulClick() {
 
-	var url = window.location.href;
-	_czc.push(["_trackEvent", "feedback-helpful", "", ""]);
-    alert("感谢您的反馈");
+	_czc.push(["_trackEvent", "feedback-helpful", "xx", "xx"]);
+  alert("感谢您的反馈");
 }
 
 function unhelpfulClick() {
-    _czc.push(['_trackEvent','feedback-unhelpful','', '']);
-    alert("感谢您的反馈");
+  _czc.push(['_trackEvent','feedback-unhelpful','xx', 'xx']);
+  alert("感谢您的反馈");
 }
 
 window.$docsify.plugins = [].concat(plugin, window.$docsify.plugins);
